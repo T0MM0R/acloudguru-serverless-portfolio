01b8e5a830b9eaf656b565fb9d5e77fc9e3d0cf8
twilson <thomasscot@gmail.com>
2017-09-19 16:19:17 -0400

    updated lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,17 +5,26 @@ import mimetypes
 
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('serverless.thomaswilson.me')
-    build_bucket = s3.Bucket('serverlessbuild.thomaswilson.me')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('serverlessbuild.zip', portfolio_zip)
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:502003314174:deployPortfolioTopic')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('serverless.thomaswilson.me')
+        build_bucket = s3.Bucket('serverlessbuild.thomaswilson.me')
 
-    print "Job finished"
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('serverlessbuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        topic.publish(Subject="portfolio deployed", Message="The portfolio was deployed to serverless.thomaswilsonme")
+
+    except:
+        topic.publish(Subject="portfolio not deployed", Message="The portfolio was not deployed to serverless.thomaswilsonme")
+        raise
