55a45e769df1db0c181fc5199fed0c5d343ea7b0
twilson <thomasscot@gmail.com>
2017-09-19 15:38:29 -0400

    added deploy script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,17 @@
+import StringIO
+import boto3
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+portfolio_bucket = s3.Bucket('serverless.thomaswilson.me')
+build_bucket = s3.Bucket('serverlessbuild.thomaswilson.me')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('serverlessbuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
