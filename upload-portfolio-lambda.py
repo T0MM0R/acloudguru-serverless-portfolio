f27e763091d6132dcb49ff843379708b25e280bd
twilson <thomasscot@gmail.com>
2017-09-19 22:10:47 -0400

    minor change to index.html

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -9,13 +9,27 @@ def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:502003314174:deployPortfolioTopic')
 
+    location = {
+        "bucketName": "serverlessbuild.thomaswilson.me"
+        "objectKey": "serverlessbuild.zip"
+    }
+
     try:
+        job = event.get('codePipeline.job')
+
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == "MyAppBuild":
+                    location = artifact['location']['s3Location']
+
+
+
         s3 = boto3.resource('s3')
         portfolio_bucket = s3.Bucket('serverless.thomaswilson.me')
-        build_bucket = s3.Bucket('serverlessbuild.thomaswilson.me')
+        build_bucket = s3.Bucket(location['bucketName'])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('serverlessbuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
