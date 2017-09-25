0f1e2dd7df2098d6ab98ae2617df302f2f7a0c7e
twilson <thomasscot@gmail.com>
2017-09-25 15:17:29 -0400

    added react components

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-import 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import ExampleWork from "./example-work";
+
+const myWork = [
+  {
+    'title': "test1",
+    'image': {
+      'desc': "this is test 1",
+      'src': "images/example1.png",
+      'comment': "blah blah"
+    }
+  },
+  {
+    'title': "test1",
+    'image': {
+      'desc': "this",
+      'src': "images/example1.png",
+      'comment': "blah blah"
+    }
+  },
+  {
+    'title': "test1",
+    'image': {
+      'desc': "this",
+      'src': "images/example1.png",
+      'comment': "blah blah"
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork} />, document.getElementById('example-work'));
